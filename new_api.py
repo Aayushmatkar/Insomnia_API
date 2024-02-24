b3c2d4bee8559653380ed3c994205aef76459ea1
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-02-24 03:17:13 -0500

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -16,6 +16,12 @@ class SleepData(BaseModel):
     stress_level: float
     physical_activity_level: float
 
+@app.post("")
+def foobar():
+    return {
+        "Server is up and Running"
+    }
+
 @app.post("/predict")
 def predict_sleep_disorder(data: SleepData):
     '''

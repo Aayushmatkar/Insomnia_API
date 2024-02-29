93394beb397b2fa1a17901951ce040f1769e4d2f
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-02-29 00:23:54 -0500

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -1,5 +1,6 @@
 import numpy as np
-from fastapi import FastAPI, HTTPException, Form
+from fastapi import FastAPI, HTTPException, Form, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sklearn.preprocessing import StandardScaler
 import pickle
@@ -9,6 +10,20 @@ app = FastAPI()
 # Load the pre-trained Random Forest Classifier model and scaler
 model = pickle.load(open('sleep_disorder_model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+# Setup CORS
+origins = [
+    "http://localhost:3000",  # Update this with the actual origin of your frontend
+    "https://insomnia2024-3f07450b7cfb.herokuapp.com/predict",  # Add additional origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SleepData(BaseModel):
     quality_of_sleep: float
